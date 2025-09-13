ac7e62c1c25aa88a1efa7c77da39d7b8987f7a2d
Soney <sonephyo7777777@gmail.com>
2025-09-13 01:43:07 -0400

    docs(cookies): move code in docs/api/cookies.rst to examples/cookies/

---
+++ examples/cookies/get_cookies_asgi.py
@@ -0,0 +1,14 @@
+class Resource:
+    async def on_get(self, req, resp):
+
+        # Get a dict of name/value cookie pairs.
+        cookies = req.cookies
+
+        # NOTE: Since get_cookie_values() is synchronous, it does
+        #   not need to be await'd.
+        my_cookie_values = req.get_cookie_values('my_cookie')
+
+        if my_cookie_values:
+            # NOTE: If there are multiple values set for the cookie, you
+            #   will need to choose how to handle the additional values.
+            v = my_cookie_values[0]
---
+++ examples/cookies/get_cookies_wsgi.py
@@ -0,0 +1,12 @@
+class Resource:
+    def on_get(self, req, resp):
+
+        # Get a dict of name/value cookie pairs.
+        cookies = req.cookies
+
+        my_cookie_values = req.get_cookie_values('my_cookie')
+
+        if my_cookie_values:
+            # NOTE: If there are multiple values set for the cookie, you
+            #   will need to choose how to handle the additional values.
+            v = my_cookie_values[0]
